c1f58dcc01949896f02f784f39e79961fdee5973
Gokul Kumarresen <gokulk@live.com>
2018-08-22 17:50:02 -0400

    added hash_request() method in Utils class to create HMAC SHA256 signatures for api requests

--- utils/utils.py
+++ utils/utils.py
@@ -1,4 +1,6 @@
 import json
+import hashlib
+import hmac
 
 
 class Utils(object):
@@ -8,3 +10,9 @@ class Utils(object):
     @staticmethod
     def to_json(data):
         return json.loads(data.text)
+
+    @staticmethod
+    def hash_request(secret, message):
+        return hmac.new(secret.encode('utf-8'),
+                        message.encode('utf-8'),
+                        hashlib.sha256).hexdigest()
