d7f0441dba6caf180ff9826dba82c1bbbeb34d65
Gokul Kumarresen <gokulk@live.com>
2018-08-25 19:45:10 -0400

    added method get_order_history() to Bittrex class to retrieve all past orders placed

--- exchanges/bittrex/bittrex.py
+++ exchanges/bittrex/bittrex.py
@@ -16,6 +16,22 @@ class Bittrex(object):
         pass
 
     def create_limit_order(self, trade_obj):
+        # endpoint = Bittrex._get_trade_signal(trade_obj.get_action())
+        # new_params = {
+        #     "market": trade_obj.get_ticker(),
+        #     "quantity": trade_obj.get_quantity(),
+        #     "rate": trade_obj.get_price()
+        # }
+        pass
+
+    @staticmethod
+    def _get_trade_signal(signal):
+        # if signal == "SELL":
+        #     return EndpointConstants.ORDER_BUY_LIMIT
+        # elif signal == "BUY":
+        #     return EndpointConstants.ORDER_BUY_LIMIT
+        # else:
+        #     return None
         pass
 
     def create_market_order(self, trade_obj):
@@ -24,21 +40,37 @@ class Bittrex(object):
     def _create_order(self, params):
         pass
 
-    def cancel_order(self, symbol, order_id):
+    def cancel_order(self, order_id, symbol=None):
         pass
 
-    def get_order_status(self, symbol, order_id):
+    def get_order_status(self, order_id, symbol=None):
+        # endpoint = EndpointConstants.GET_ORDER
+        # new_params = {
+        #     "uuid": order_id
+        # }
+        #
+        # return self._make_request(endpoint, new_params)
         pass
+
+    def get_order_history(self, symbol=None):
+        endpoint = EndpointConstants.GET_ORDER_HISTORY
+        new_params = {}
+        if symbol is not None:
+            new_params["market"] = symbol
+
+        return self._make_request(endpoint, new_params)
 
     def get_balance(self, asset):
         endpoint = EndpointConstants.GET_BALANCE
         new_params = {
             "currency": asset
         }
+
         return self._make_request(endpoint, new_params)
 
     def get_all_balances(self):
         endpoint = EndpointConstants.GET_ALL_BALANCES
+
         return self._make_request(endpoint)
 
     def _make_request(self, endpoint, new_params=None):
